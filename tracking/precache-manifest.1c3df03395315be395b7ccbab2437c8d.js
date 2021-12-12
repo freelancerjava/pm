7991ff1287fd7594c855eaef1d30ea65df860745
Programmer <iam@programmer.net>
2021-12-12 14:09:38 +0500

    pm

---
+++ tracking/precache-manifest.1c3df03395315be395b7ccbab2437c8d.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "2406a510abb241355602",
+    "url": "/tracking/static/css/main.a1f80457.chunk.css"
+  },
+  {
+    "revision": "2406a510abb241355602",
+    "url": "/tracking/static/js/main.2406a510.chunk.js"
+  },
+  {
+    "revision": "5956886ce5b85e718b76",
+    "url": "/tracking/static/css/1.5a41249c.chunk.css"
+  },
+  {
+    "revision": "5956886ce5b85e718b76",
+    "url": "/tracking/static/js/1.5956886c.chunk.js"
+  },
+  {
+    "revision": "fd1959b860315948fe7c",
+    "url": "/tracking/static/js/runtime~main.fd1959b8.js"
+  },
+  {
+    "revision": "f2c9b1c4ea8c2e96fd53c281c0b1bfa0",
+    "url": "/tracking/static/media/check.f2c9b1c4.svg"
+  },
+  {
+    "revision": "d4bb2af20155f6b834dcbf1cfd513020",
+    "url": "/tracking/static/media/call.d4bb2af2.svg"
+  },
+  {
+    "revision": "b30a357caa4ca988b1740a298430d80a",
+    "url": "/tracking/static/media/back.b30a357c.svg"
+  },
+  {
+    "revision": "9054b7e7b4422b2abdb8806a77180a11",
+    "url": "/tracking/static/media/more.9054b7e7.svg"
+  },
+  {
+    "revision": "1c2ea5123dec29f5aa6343f61065d5b3",
+    "url": "/tracking/static/media/send.1c2ea512.svg"
+  },
+  {
+    "revision": "fd56edb6befa9213d4303349b86efaf7",
+    "url": "/tracking/static/media/chat-bg.fd56edb6.svg"
+  },
+  {
+    "revision": "5c097c20e6c8c402a3a343b7c126ca12",
+    "url": "/tracking/static/media/logo.5c097c20.png"
+  },
+  {
+    "revision": "46201fca3941a16bfa2b76f2e63d5f3b",
+    "url": "/tracking/static/media/tuzumi.46201fca.jpeg"
+  },
+  {
+    "revision": "25b1f144ba9c21576df11731c5b68e51",
+    "url": "/tracking/static/media/nimagaaap.25b1f144.jpeg"
+  },
+  {
+    "revision": "6de482646c646ef18a34bfe8d3eea028",
+    "url": "/tracking/static/media/Iphone.6de48264.jpeg"
+  },
+  {
+    "revision": "2ec936082e3568a1b0d009bb1efdfb47",
+    "url": "/tracking/static/media/pm-logo.2ec93608.png"
+  },
+  {
+    "revision": "50c8b0e8080522b47234a66f008c55cc",
+    "url": "/tracking/static/media/peterbilt.50c8b0e8.png"
+  },
+  {
+    "revision": "d6b8c4d70c4bd37d3bae6f5ef31b9c08",
+    "url": "/tracking/index.html"
+  }
+];
